ceb7a6e3c704f675dc90216eca261e107ea52549
Vladimov Ivanov <vovan_iva@mail.ru>
2021-04-15 19:49:03 +0300

    f

---
+++ js/filter__slider.js
@@ -0,0 +1,36 @@
+const filterSlider = document.getElementById('filter-slider');
+if(filterSlider){
+  noUiSlider.create(filterSlider, {
+    start: [500, 300000],
+    connect: true,
+    tooltips: [ wNumb({decimals: 1}), wNumb({decimals: 1})],
+    step:1,
+    range: {
+        'min': [500],
+        'max': [300000]
+    }
+});
+  const input0 = document.getElementById('input-0');
+  const input1 = document.getElementById('input-1');
+  const inputs = [input0, input1];
+  filterSlider.noUiSlider.on('update', function(values,handle){
+    inputs[handle].value = Math.round(values[handle]);
+  
+});
+
+const setFilterSlide = (i,value) =>{
+  let arr = [ null , null];
+  arr[i] = value;
+
+  console.log(arr)
+
+  filterSlider.noUiSlider.set(arr);
+}
+
+  inputs.forEach((el, index) => {
+  el.addEventListener('change',(e) =>{
+    console.log(index);
+    setFilterSlide(index,e.currentTarget.value)
+  });
+});
+}
