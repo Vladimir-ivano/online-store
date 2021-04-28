e068a205a2137e6bd63c9c9a316db92b6872782f
Vladimov Ivanov <vovan_iva@mail.ru>
2021-04-28 21:56:11 +0300

    xcxc

--- js/burger.js
+++ js/burger.js
@@ -2,7 +2,7 @@
 
 const iconMenu = document.querySelector( ' .burger');
 if(iconMenu ) {
-  const iconBody = document.querySelector( '.nav__nav ');
+  const iconBody = document.querySelector( '.nav__navv ');
   iconMenu .addEventListener("click", function(e){
     iconMenu.classList.toggle( 'activ')
     iconBody.classList.toggle( 'activ')
