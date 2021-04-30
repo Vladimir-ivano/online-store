56e729dc02487feb66e82c73de47e66e8d99f030
Vladimov Ivanov <vovan_iva@mail.ru>
2021-04-30 19:21:28 +0300

    g

--- js/burger.js
+++ js/burger.js
@@ -1,10 +1,10 @@
 //меню бургера
 
-const iconMenu = document.querySelector( ' .burger');
-if(iconMenu ) {
-  const iconBody = document.querySelector( '.nav__navigation');
-  iconMenu .addEventListener("click", function(e){
-    iconMenu.classList.toggle( 'activ')
+const iconsMenu = document.querySelector( ' .burger');
+if(iconsMenu ) {
+  const iconBody = document.querySelector( '.navigation__nav ');
+  iconsMenu .addEventListener("click", function(e){
+    iconsMenu.classList.toggle( 'activ')
     iconBody.classList.toggle( 'activ')
   });
 }
