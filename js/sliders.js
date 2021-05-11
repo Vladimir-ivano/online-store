13e88de49efb6b4416c417dd88b41932d31f59c5
Vladimov Ivanov <vovan_iva@mail.ru>
2021-05-11 21:47:32 +0300

    d

--- js/sliders.js
+++ js/sliders.js
@@ -1,21 +1,68 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+
+let sliderBody = new Swiper('.swiper-container', {
+  /*
+    effect: 'fade',
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    */
+    observer:true,
+    observerParents: true,
+    slidesPerView: 1,
+    autoHeight: true,
+    speed: 800,
+    spaceBetween: 0,
+    loop: true,
+    //touchRatio: 0 ,
+    //simuLateTouch: false,
+    //Lazy: true
+    //Dotts
+    pagination: {
+      el: '.slider__dotts',
+      clickable: true,
+    },
+    
+    //Arrws
+    /*
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    */
+    /*
+      breakpoints:{
+        320:{
+          slidesPerView: 1,
+          autoHeight: true,
+          spaceBetween: 0,
+        },
+        720{
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        992{
+          spaceBetween: 20,
+          slidesPerView: 3,
+        },
+        1286{
+          spaceBetween: 30,
+          slidesPerView: 4,
+        },
+
+      },
+    */
+
+    on:{
+      lazyImageReady: function(){
+        ibg();
+      },
+    }  
+    // And if we need scrollbar
+  //scrollbar: {
+    //el: '.swiper-scrollbar',
+  //},
 });
