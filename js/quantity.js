ebd6da9b4b35dd6076408f579edf92e03c192255
Vladimov Ivanov <vovan_iva@mail.ru>
2021-06-24 17:59:20 +0300

    всч

--- js/quantity.js
+++ js/quantity.js
@@ -1,27 +1,35 @@
-const formatNumber = (x) => x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ' ');
+const orderItems = document.querySelectorAll('.order__items'), // Все карточки корзины.
+	totalSum = document.querySelector('#total-prise'); // Итоговая сумма всех товаров в корзине.
 
-const totalPriseWrapper = document.getElementById('total-prise');
+// Установка начального значения итоговой суммы товаров в корзине, если оно не установлено.
+if (orderItems.length === 0) totalSum.textContent = 0;
 
-const getItemSubTotalPrice = (inputs) => Number(inputs.value) * Number(inputs.dataset.prise);
+// Переберем все имеющиеся карточки и навесим функционала на каждую:
+for (let oi = 0; oi < orderItems.length; oi++) {
+	const orderItem = orderItems[oi], // Карточка товара.
+		inputCount = orderItem.querySelector('.quantity__inputs'), // Поле количества штук этого товара.
+		productPrice = +inputCount.getAttribute('data-prise'), // Цена товара.
+		btnMinus = orderItem.querySelector('.quantity__button-minus'), // Кнопка уменьшения количества.
+		btnPlus = orderItem.querySelector('.quantity__button-plus'), // Кнопка увеличения количества.
+		totalPrice = orderItem.querySelector('.order__total__prise'); // Итоговая цена товара.
 
-const init = () =>{
-  let totalCost = 0;
+	let counterItem = 1; // Начальное количество товара.
 
-  [ ... document.querySelectorAll('.order__items')].forEach((orderItem)=>{
-    
-    totalCost += getItemSubTotalPrice(orderItem.querySelector('.input'))  ;
-  });
-  totalPriseWrapper.textContent = formatNumber (totalCost);
+	// Установка начального значения количества штук товара, если оно не установлено.
+	if (inputCount.value === '' || inputCount.value === 0) inputCount.value = counterItem;
 
+	// Изменение итоговой суммы исходя от выбранных товаров:
+	if ((totalSum.textContent === '' || totalSum.textContent === 0) && orderItems.length > 0) totalSum.textContent += productPrice;
+
+	// Уменьшение количества штук товара и уменьшение общей цены за товар:
+	btnMinus.addEventListener('click', function() {
+		counterItem <= 1 ? counterItem = 1 : counterItem--;
+		totalPrice.textContent = productPrice*counterItem;
+	});
+
+	// Увеличение количества штук товара и увеличение общей цены за товар:
+	btnPlus.addEventListener('click', function() {
+		counterItem++;
+		totalPrice.textContent = productPrice*counterItem;
+	});
 };
-
-init();
-
-document.getElementById('basket').addEventListener('click', (event)=> {
-  if (event.target.classList.contains('quantity__button-minus')){
-    console.log('minus');
-  }
-  if (event.target.classList.contains('quantity__button-minus')){
-    console.log('plus');
-  }
-});
